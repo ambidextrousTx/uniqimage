039f0bef5d451c20db787ad88d13b0d09987e170
Ravi Sinha <ravisinha@my.unt.edu>
2025-11-25 18:59:42 -0600

    Add ability to scan an input folder for images

---
+++ src/scanner.py
@@ -0,0 +1,36 @@
+import argparse
+from pathlib import Path
+
+
+def is_image_file(path):
+    return path.suffix.lower() in {'.jpg', '.jpeg', '.png'}
+
+
+def can_scan_folder(folder):
+    if not folder.exists():
+        print(f"Error: Folder '{folder}' does not exist")
+        return False
+    if not folder.is_dir():
+        print(f"Error: '{folder}' is not a directory")
+        return False
+    return True
+
+
+def main():
+    parser = argparse.ArgumentParser(
+            description='Identify duplicate images in a folder')
+    parser.add_argument('folder', type=str, help='the target folder')
+    args = parser.parse_args()
+    input_folder = args.folder
+    print(f'Scanning {input_folder} ...')
+    target_directory = Path(input_folder)
+    if not can_scan_folder(target_directory):
+        return
+
+    image_files = [f for f in target_directory.rglob('*') if is_image_file(f)]
+    print(f'Found {len(image_files)} files')
+    print(image_files)
+
+
+if __name__ == "__main__":
+    main()
