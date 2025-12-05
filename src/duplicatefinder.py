9d53bd3248dfb62e95dc36831517f2291fae84fe
Ravi Sinha <ravisinha@my.unt.edu>
2025-12-04 19:07:10 -0600

    Get tqdm progress bar to show progress as workers are computing hashes

--- src/duplicatefinder.py
+++ src/duplicatefinder.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from tqdm import tqdm
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import imagehash
 import logging
 
@@ -20,15 +20,20 @@ def compute_image_hash(image_path):
 def compute_image_hashes_concurrently(image_paths):
     hashes_to_paths = {}
     logger.info(f"Computing hashes for {len(image_paths)} images...")
-    with ProcessPoolExecutor() as executor:
-        results = executor.map(compute_image_hash, image_paths)
 
-        # Each process in the pool gets its own memory, so we cannot
-        # pass the hashes_to_paths. With multiprocessing, it is cleaner
-        # to distribute the work of computing hashes and then combine
-        # the results in the caller as done here
-        for image_path, image_hash in tqdm(results, total=len(image_paths),
-                                           desc="Computing image hashes"):
+    # Each process in the pool gets its own memory, so we cannot
+    # pass the hashes_to_paths to the workers. With multiprocessing,
+    # it is cleaner to distribute the work of computing hashes and
+    # then combine the results in the caller as done here
+    with ProcessPoolExecutor() as executor:
+        # A simpler solution of using `executor.map` causes tqdm to
+        # remain quiet while the workers are computing image hashes
+        # This approach is meant to show a progress bar even as the
+        # multiprocessing workers are completing because it processes
+        # results as they complete
+        futures = {executor.submit(compute_image_hash, path): path for path in image_paths}
+        for future in tqdm(as_completed(futures), total=len(image_paths), desc="Computing hashes"):
+            image_path, image_hash = future.result()
             if image_hash is not None:
                 if image_hash not in hashes_to_paths:
                     hashes_to_paths[image_hash] = []
